a9330bd9f24397a967f9ddc7f7512823239c9ca3
Flavius G <62383007+flaviusmax@users.noreply.github.com>
2023-06-23 19:24:52 +0300

    a doua schimbare
    a doua schimbare in REMOTE REPO

--- index.php
+++ index.php
@@ -5,3 +5,7 @@ echo "acesta este pagina: $indexPage";
 //code nou sa vad cum merge commitul din REMOTE MAIN=> in LOCAL REPO
 
 echo "teste LOCAL";
+
+//mai fac o schimbare
+
+echo "<br>a doua schimbare";
