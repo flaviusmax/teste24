ef6edfb5ed65919e8abd5f0edf36dc453970c1cc
Flavius G <flaviusmaximus@gmail.com>
2023-06-23 19:17:33 +0300

    am modificat fisierul teste2

--- test/teste2.php
+++ test/teste2.php
@@ -1,8 +1,8 @@
 <?php
  $teste2 = "pagina teste.php";
 //teste cinci
+$test5= 5;
 
+echo "Teste doi  $test5 <br>" ;
 
-echo "Teste doi  $test" ;
-
-echo "aceasta este "; 
+echo "aceasta este  $teste2 si este egala cu cifra: $test5= 5;  "; 
